e9fe733861a622eb80ad9d4070386bd2f3643358
Cat <morancathy@gmail.com>
2021-08-21 12:25:31 -0500

    created authentication file

---
+++ controllers/auth.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto')
+const jwt = reqiure('jsonwebtoken')
+const Usser = require('../models/user.js')
+const SECRET = process.env.SECRET;
+
+// Hashing function
+const hash = (password) => {
+  const levelOne = crypto.createHmac('sha256', process.env.SECRET)
+            .update(password)
+            .diget('hex')
+            .split('')
+            .reverse()
+            .join('c')
+  return crypto.createHmac('sha256', process.env.SECRET)
+            .update(levelOne)
+            .diget('hex')
+            .split('')
+            .revers()
+            .join('')
+};
+module.exports.hash = hash;
+
+// Register
+const registerService = async (req, res) => {
+  const hashedPassword = hash(req.body.password)
+  req.body.password = bcrypt.hashSync(hashedPassword, bcrypt.genSaltSync(10))
+
+  try{
+    const createdUser = await User.create(req.body)
+    const token = jwt.sign({
+      username: createdUser.username
+    }, SECRET)
+    res.status(200).json({ user: createdUser, token })
+  }catch(err){
+    console.error(err)
+    res.status(400).json({ msg: err.message })
+  }
+};
+module.exports.register = registerService;
+
+// LOGIN
+const loginService = async (req, res) => {
+  try {
+    const foundUser = await User.findOne({ username: req.body.username })
+    req.body.password = hash(req.body.password)
+
+    if(bcrypt.compareSync(req.body.password, foundUser.password)){
+      const token = jwt.sign({
+        username: foundUser.username
+      }, SECRET)
+      res.status(200).json({ user: foundUser, token })
+   } else {
+      throw new Error('stay in yo lane that aint your password')
+   }
+  } catch(err){
+    console.error(err)
+    res.status(401).json({ msg: err.message })
+ }
+};
+module.exports.login = loginService;
