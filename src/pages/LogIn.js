d2f64abab7423ff98f7a54e9438cd350c8d1b996
Cat <morancathy@gmail.com>
2021-08-21 21:28:30 -0500

    log in successful

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -3,8 +3,57 @@ import { Link } from 'react-router-dom';
 
 const LogIn = props => {
 	const [token, setToken] = useState('');
+	const [user, setUser] = useState({
+		username: '',
+		password: ''
+	});
 
-	return <div className="LogIn">This is the Login Page</div>;
+	const handleChange = e => {
+		setUser({ ...user, [e.target.id]: e.target.value });
+	};
+
+	const handleLogin = async e => {
+		e.preventDefault();
+		try {
+			const response = await fetch('/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(user)
+			});
+			const data = await response.json();
+			setToken(data.token);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<div className="LogIn">
+			{!token ? (
+				<>
+					<form onSubmit={handleLogin}>
+						<input
+							type="text"
+							id="username"
+							value={user.username}
+							onChange={handleChange}
+						/>
+						<input
+							type="text"
+							id="password"
+							value={user.password}
+							onChange={handleChange}
+						/>
+						<input type="submit" value="Log In" />
+					</form>
+				</>
+			) : (
+				<div>hello</div>
+			)}
+		</div>
+	);
 };
 
 export default LogIn;
